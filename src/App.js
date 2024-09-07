7d5161327c8efa8ff4aced1ec2bf76c4011265fc
adithkrishnaor <adithkrishnaor@gmail.com>
2024-09-07 22:39:47 +0530

    Home page just created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path='/' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<HomePage />} />
       </Routes>
     </BrowserRouter>
 
