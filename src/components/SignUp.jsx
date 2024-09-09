09851b923ef7cd29d0367f1b16349713f5562ad3
adithkrishnaor <adithkrishnaor@gmail.com>
2024-09-09 12:43:07 +0530

    login and signup design created and blank form fill validated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -21,6 +21,11 @@ const SignUp = () => {
 
     const readValue = () => {
 
+        if (!data.name || !data.email || !data.phone || !data.dob || !data.gender || !data.password || !data.cpass) {
+            alert('Please fill in all fields.');
+            return;
+        }
+
         if (data.password == data.cpass) {
 
             let newdata = {
@@ -39,13 +44,13 @@ const SignUp = () => {
                     if (response.data.status == "success") {
                         alert("Registration Success")
                         setData(
-                            { "name": "","email": "","phone": "","dob": "","gender": "","password": "","cpass": "" }
+                            { "name": "", "email": "", "phone": "", "dob": "", "gender": "", "password": "", "cpass": "" }
                         )
                     }
                     else {
                         alert("User already exist")
                         setData(
-                            { "name": "","email": "","phone": "","dob": "","gender": "","password": "","cpass": "" }
+                            { "name": "", "email": "", "phone": "", "dob": "", "gender": "", "password": "", "cpass": "" }
                         )
                     }
                 }
@@ -62,48 +67,57 @@ const SignUp = () => {
     }
     return (
         <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Full Name</label>
-                                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Email</label>
-                                <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Phone Number</label>
-                                <input type="number" className="form-control" name='phone' value={data.phone} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Date of Birth</label>
-                                <input type="date" className="form-control" name='dob' value={data.dob} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Gender</label>
-                                <select className="form-control" name='gender' value={data.gender} onChange={inputHandler}>
-                                    <option value="">Select Gender</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                </select>
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Password</label>
-                                <input type="text" className="form-control" name='password' value={data.password} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Confirm Password</label>
-                                <input type="text" className="form-control" name='cpass' value={data.cpass} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <button className="btn btn-success" onClick={readValue}>Submit</button>
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <a href="/" className="btn btn-primary">Existing user Login Page</a>
+            <div className="container-fluid bg-light min-vh-100 d-flex align-items-center">
+                <div className="row justify-content-center">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 d-flex justify-content-center">
+                        <div className="card w-50 border-rounded border-secondary shadow-sm">
+                            <div className="card-body p-4">
+                                <h2 className="card-title text-center mb-4">Sign Up</h2>
+                                <div className="row g-3">
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Full Name</label>
+                                        <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Email</label>
+                                        <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Phone Number</label>
+                                        <input type="number" className="form-control" name='phone' value={data.phone} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Date of Birth</label>
+                                        <input type="date" className="form-control" name='dob' value={data.dob} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Gender</label>
+                                        <select className="form-control" name='gender' value={data.gender} onChange={inputHandler}>
+                                            <option value="">Select Gender</option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Password</label>
+                                        <input type="password" className="form-control" name='password' value={data.password} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <label htmlFor="" className="form-label">Confirm Password</label>
+                                        <input type="password" className="form-control" name='cpass' value={data.cpass} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <center>
+                                            <button className="btn btn-primary" onClick={readValue}>Signup</button>
+                                        </center>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <center>
+                                            <a href="/">Existing user Login Page</a>
+                                        </center>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
